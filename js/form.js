d96b5db8c1ffdbaef880594097f6184376212d39
lenkoom <64737220+lenkoom@users.noreply.github.com>
2020-07-14 01:18:03 +0300

    Правки к 3 защите

--- js/form.js
+++ js/form.js
@@ -5,40 +5,35 @@ var inputname = modalform.querySelector(".input-name");
 var inputemail = modalform.querySelector(".input-email");
 var isStorageSupport = true;
 var storage = "";
-
 try {
   storage = localStorage.getItem("input-name");
 } catch (err) {
   isStorageSupport = false;
 }
-
-modallink.addEventListener("click", function (evt) {
+modallink.addEventListener("click", function(evt) {
   evt.preventDefault();
   modalform.classList.add("modal-show");
   if (storage) {
     inputname.value = storage;
     inputemail.focus();
   } else {
-  username.focus();
+    username.focus();
   }
 });
-
-modalclose.addEventListener("click", function (evt) {
+modalclose.addEventListener("click", function(evt) {
   evt.preventDefault();
   modalform.classList.remove("modal-show");
 });
-
-modalform.addEventListener("submit", function (evt) {
+modalform.addEventListener("submit", function(evt) {
   if (!inputname.value || !inputemail.value) {
     evt.preventDefault();
-} else {
-  if (isStorageSupport) {
+  } else {
+    if (isStorageSupport) {
       localStorage.setItem("input-name", inputname.value);
     }
   }
 });
-
-window.addEventListener("keydown", function (evt) {
+window.addEventListener("keydown", function(evt) {
   if (evt.keyCode === 27) {
     if (modalform.classList.contains("modal-show")) {
       evt.preventDefault();
